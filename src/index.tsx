3ef62730fdf8c94773459886c1355df991e28616
pawel kozubek <pkozubek92@gmail.com>
2020-11-04 19:48:38 +0100

    working with webpack

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <div>pixelart</div>;
+    return <div>pixelart
+        <p>zmiana</p>
+        <p>zmiana2</p>
+    </div>;
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,75 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const { WebpackPluginServe: ServePlugin } = require('webpack-plugin-serve');
+
+const outputPath = path.resolve(__dirname, 'build');
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        app: path.join('src', 'index.tsx')
-    },
-    target: 'web',
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    },
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: 'ts-loader',
-          exclude: '/node_modules/'
+  mode: 'development',
+  entry: {
+    main: ['webpack-plugin-serve/client', './src/index.tsx'],
+  },
+  output: {
+    path: outputPath,
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            cacheDirectory: true,
+            babelrc: false,
+            presets: [
+              [
+                "@babel/preset-env",
+                { targets: { browsers: "last 2 versions" } } // or whatever your project requires
+              ],
+              "@babel/preset-typescript",
+              "@babel/preset-react"
+            ],
+            plugins: [
+              // plugin-proposal-decorators is only needed if you're using experimental decorators in TypeScript
+              ["@babel/plugin-proposal-decorators", { legacy: true }],
+              ["@babel/plugin-proposal-class-properties", { loose: true }],
+              require.resolve('react-refresh/babel')
+            ]
+          }
         }
-      ],
-    },
-    output: {
-      filename: '[name].js',
-      path: path.resolve(__dirname, 'dist')
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-          template: path.join(__dirname, 'src', 'index.html')
-      })
+      }, {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
     ]
-  };
+  },
+  plugins: [
+    new ServePlugin({
+      static: outputPath,
+      status: false,
+    }),
+    new ReactRefreshWebpackPlugin({
+      overlay: {
+        sockIntegration: 'wps',
+      },
+    }),
+    new HtmlWebpackPlugin({
+      title: "Pixelart",
+      template: './index.html',
+    }),
+    new ForkTsCheckerWebpackPlugin()
+  ],
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  }
+};
