15bb7e9d67d16652d443c420366f6fede986b5f7
pawel kozubek <pkozubek92@gmail.com>
2020-11-04 20:18:19 +0100

    working on fast refresh

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import "./app.scss"
+
 const App = () => {
     return <div>pixelart
         <p>zmiana</p>
-        <p>zmiana2</p>
+        zzsa
     </div>;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const { WebpackPluginServe: ServePlugin } = require('webpack-plugin-serve');
 
 const outputPath = path.resolve(__dirname, 'build');
@@ -20,28 +19,8 @@ module.exports = {
     rules: [
       {
         test: /\.(j|t)sx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            cacheDirectory: true,
-            babelrc: false,
-            presets: [
-              [
-                "@babel/preset-env",
-                { targets: { browsers: "last 2 versions" } } // or whatever your project requires
-              ],
-              "@babel/preset-typescript",
-              "@babel/preset-react"
-            ],
-            plugins: [
-              // plugin-proposal-decorators is only needed if you're using experimental decorators in TypeScript
-              ["@babel/plugin-proposal-decorators", { legacy: true }],
-              ["@babel/plugin-proposal-class-properties", { loose: true }],
-              require.resolve('react-refresh/babel')
-            ]
-          }
-        }
+        include: path.join(__dirname, 'src'),
+        use: "babel-loader",
       }, {
         test: /\.scss$/,
         use: [
@@ -65,11 +44,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "Pixelart",
       template: './index.html',
-    }),
-    new ForkTsCheckerWebpackPlugin()
+    })
   ],
   resolve: {
-    modules: ['node_modules'],
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
   }
 };
