e16882ac808ddebfc3ca4c29a165386a4fb4654b
pawel kozubek <pkozubek92@gmail.com>
2021-01-09 20:04:44 +0100

    quick fix, add abbility to color multiple tiles

--- src/PixelGrid/PixelGrid.tsx
+++ src/PixelGrid/PixelGrid.tsx
@@ -1,22 +1,41 @@
-import React, { useContext } from 'react';
-import ColorContext from '../context/ColorContext';
+import React, { useContext, useState } from "react";
+import ColorContext from "../context/ColorContext";
 import { PixelContext } from "../context/PixelContext";
 import { StyledPixelGrid } from "./StyledPixelGrid";
 
 const PixelGrid = () => {
-    const { pickedColor } = useContext(ColorContext);
-    const { pixelTable, rows, columns, pixelSize, setPixel } = useContext(PixelContext);
+  const { pickedColor } = useContext(ColorContext);
+  const { pixelTable, rows, columns, pixelSize, setPixel } = useContext(
+    PixelContext
+  );
 
-    return <StyledPixelGrid.PixelGrid rows={rows} columns={columns} pixelSize={pixelSize}>
-        {pixelTable.map((pixelRow, columnIndex) =>
-            pixelRow.map((pixelColor, rowIndex) =>
-                <StyledPixelGrid.Pixel
-                    onClick={() => setPixel(pickedColor[0], rowIndex, columnIndex)}
-                    onContextMenu={() => setPixel(pickedColor[1], rowIndex, columnIndex)}
-                    pixelSize={pixelSize}
-                    color={pixelColor} />))}
+  const [isMouseClicked, setMouseClicked] = useState(false);
+
+  const onMouseOverTile = (rowIndex, columnIndex) => {
+    if (isMouseClicked && pixelTable[columnIndex][rowIndex] !== pickedColor[0])
+      setPixel(pickedColor[0], rowIndex, columnIndex);
+  };
+
+  return (
+    <StyledPixelGrid.PixelGrid
+      rows={rows}
+      columns={columns}
+      pixelSize={pixelSize}
+      onMouseDown={() => setMouseClicked(true)}
+      onMouseUp={() => setMouseClicked(false)}
+    >
+      {pixelTable.map((pixelRow, columnIndex) =>
+        pixelRow.map((pixelColor, rowIndex) => (
+          <StyledPixelGrid.Pixel
+            onClick={() => setPixel(pickedColor[0], rowIndex, columnIndex)}
+            onMouseMove={() => onMouseOverTile(rowIndex, columnIndex)}
+            pixelSize={pixelSize}
+            color={pixelColor}
+          />
+        ))
+      )}
     </StyledPixelGrid.PixelGrid>
-}
-
+  );
+};
 
 export default PixelGrid;
