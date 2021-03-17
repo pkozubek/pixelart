2cbffef56e46d663adb6fd0ffee18bc6f8d56f89
pawel kozubek <pkozubek92@gmail.com>
2021-03-17 18:15:18 +0100

    adding tests

---
+++ src/__tests__/PixelGrid.test.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+
+import PixelGrid from "../PixelGrid/PixelGrid";
+
+describe("<PixelGrid />", () => {
+  test("test", async () => {});
+});
